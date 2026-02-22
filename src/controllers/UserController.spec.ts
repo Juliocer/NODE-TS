4ddfb621c76130bfd64091abbcd453cd7415ceb2
Juliocer <contatodejuliocesar@gmail.com>
2026-02-22 19:27:52 -0300

    estudando test unitario na pratica atualizando

--- src/controllers/UserController.spec.ts
+++ src/controllers/UserController.spec.ts
@@ -5,6 +5,7 @@ import { Request } from "express";
 
 const mockUserService = {
     createUser: jest.fn(),
+    getAllUsers: jest.fn(),
     deleteUser: jest.fn()
 }
 
@@ -33,31 +34,82 @@ describe('UserController', () => {
         expect(mockResponse.state.json).toMatchObject({ message: expectedMessage })
     })
 
-    it('Deve retornar status 200 ao deletar usuário com sucesso', async () => {
-        const mockRequest = {
-            body: { id_user: '12345' }
-        } as Request
+    describe('getAllusers', () => {
+        it('Deve retornar 200 e lista de usuários com sucesso', async () => {
+            const mockUsers = [
+                { id_user: '1', name: 'Julio', email: 'julio@gmail.com', password: '123456' },
+                { id_user: '2', name: 'Maria', email: 'maria@gmail.com', password: '654321' }
+            ]
 
-        const mockResponse = makeMockResponse();
+            mockUserService.getAllUsers = jest.fn().mockResolvedValue(mockUsers)
 
-        await userController.deleteUser(mockRequest, mockResponse);
+            const mockRequest = {} as Request
+            const mockResponse = makeMockResponse()
 
-        expect(mockResponse.state.status).toBe(200);
-        expect(mockResponse.state.json).toMatchObject({ message: 'Usuário deletado' });
-        expect(mockUserService.deleteUser).toHaveBeenCalledWith('12345');
-    });
+            await userController.getAllUsers(mockRequest, mockResponse)
 
-    it('Deve retornar status 400 quando o id_user não for informado', async () => {
-        const mockRequest = {
-            body: {}
-        } as Request
+            expect(mockUserService.getAllUsers).toHaveBeenCalled()
+            expect(mockResponse.state.status).toBe(200)
+            expect(mockResponse.state.json).toMatchObject(mockUsers)
+        })
 
-        const mockResponse = makeMockResponse();
+        it('Deve retornar status 500 quando ocorrer um erro', async () => {
+            mockUserService.getAllUsers = jest.fn().mockRejectedValue(new Error('Erro no banco'))
 
-        await userController.deleteUser(mockRequest, mockResponse);
+            const mockRequest = {} as Request
+            const mockResponse = makeMockResponse()
 
-        expect(mockResponse.state.status).toBe(400);
-        expect(mockResponse.state.json).toMatchObject({ message: 'Bad request: ID é obrigatório' })
+            await userController.getAllUsers(mockRequest, mockResponse)
+
+            expect(mockResponse.state.status).toBe(500)
+            expect(mockResponse.state.json).toMatchObject({ message: 'Erro ao buscar usuários' })
+        })
+    })
+
+    describe('deleteUser', () => {
+
+        it('Deve retornar status 200 ao deletar usuário com sucesso', async () => {
+            const mockRequest = {
+                body: { id_user: '12345' }
+            } as Request
+
+            const mockResponse = makeMockResponse();
+
+            await userController.deleteUser(mockRequest, mockResponse);
+
+            expect(mockResponse.state.status).toBe(200);
+            expect(mockResponse.state.json).toMatchObject({ message: 'Usuário deletado' });
+            expect(mockUserService.deleteUser).toHaveBeenCalledWith('12345');
+        });
+
+        it('Deve retornar status 400 quando o id_user não for informado', async () => {
+            const mockRequest = {
+                body: {}
+            } as Request
+
+            const mockResponse = makeMockResponse();
+
+            await userController.deleteUser(mockRequest, mockResponse);
+
+            expect(mockResponse.state.status).toBe(400);
+            expect(mockResponse.state.json).toMatchObject({ message: 'Bad request: ID é obrigatório' })
+        })
+
+        it('Deve retornar status 500 quando ocorrer um erro ao deletar', async () => {
+            mockUserService.deleteUser = jest.fn().mockRejectedValue(new Error('Erro no banco'))
+
+            const mockRequest = {
+                body: { id_user: '12345' }
+            } as Request
+
+            const mockResponse = makeMockResponse();
+
+            await userController.deleteUser(mockRequest, mockResponse)
+
+            expect(mockResponse.state.status).toBe(500)
+            expect(mockResponse.state.json).toMatchObject({ message: 'Erro ao deletar usuário' })
+        })
+
     })
 
 })
